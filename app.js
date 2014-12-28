845ff990334f34b1f7b1cca2e4797ca92a005266
xinlei <wang.xinlei15@gmail.com>
2014-12-27 18:24:33 -0600

    completed routing

---
+++ app.js
@@ -0,0 +1,46 @@
+angular.module('flapperNews', ['ui.router'])
+.config([
+	'$stateProvider',
+	'$urlRouterProvider',
+	function($stateProvider, $urlRouterProvider){
+		$stateProvider
+			.state('home', {
+				url: '/home',
+				templateUrl: '/home.html',
+				controller: 'MainCtrl'
+			});
+		$urlRouterProvider.otherwise('home');
+	}
+])
+.factory('posts', [function(){
+	var o = {
+		posts: []
+	};
+	return o;
+}])
+.controller('MainCtrl', [
+	'$scope', 'posts',
+	function($scope, posts){
+		$scope.posts = posts.posts;
+		$scope.test = 'Hello world!';
+		$scope.posts = [
+			{title: 'post 1', upvotes: 5},
+  			{title: 'post 2', upvotes: 2},
+  			{title: 'post 3', upvotes: 15},
+  			{title: 'post 4', upvotes: 9},
+  			{title: 'post 5', upvotes: 4}
+		];
+		$scope.addPost = function(){
+			if(!$scope.title || $scope.title === ''){ return; }
+			$scope.posts.push({
+				title: $scope.title,
+				link: $scope.link,
+				upvotes: 0});
+			$scope.title = '';
+			$scope.link = '';
+		}
+		$scope.incrementUpvotes = function(post){
+			post.upvotes += 1;
+		}
+	}
+]); 
